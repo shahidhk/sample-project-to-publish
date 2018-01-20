507499dc6472353092ea4d2ebe6826119e4d7e1a
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2018-01-20 17:43:27 +0530

    Update server.py

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -4,7 +4,7 @@ from flask import jsonify
 
 @app.route("/")
 def home():
-    return "Hasura Hello World edited aaaabbbb"
+    return "Hasura Hello edited aaaabbbb"
 
 # Uncomment to add a new URL at /new
 
